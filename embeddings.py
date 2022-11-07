a885745eed30e0e7274d5ff76ef743b5e75113e6
alvinary <alvaro.cuestas.ll@gmail.com>
2022-11-07 16:06:15 -0300

    Fix negation embedding

--- embeddings.py
+++ embeddings.py
@@ -1,10 +1,11 @@
-from formulas import *
+from tale.formulas import *
 
 def negation(atoms):
     notPrefix = 'not'
     for atom in atoms:
         terms = list(atom.terms)
-        terms[0] = f"{notPrefix} {terms[0]}"
+        negatedPredicate = Term(f"{notPrefix} {terms[0].term}", [])
+        terms = [negatedPredicate] + terms[1:]
         negatedAtom = Atom(terms)
         yield Either([atom, negatedAtom])
 
