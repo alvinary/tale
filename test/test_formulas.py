ef665ffa7cef9bf07d823b32722a62907eb6251e
alvinary <alvaro.cuestas.ll@gmail.com>
2022-11-07 13:46:23 -0300

    Add test suite for .show() methods

--- test/test_formulas.py
+++ test/test_formulas.py
@@ -59,3 +59,44 @@ def test_terms():
     term4 = Term("h", "f")
     val3, err3 = term4.evaluate(index, assign)
     assert isinstance(err3, Exception)
+
+def test_rules():
+    
+    rule1s = "p(a, b) => q(a)"
+    rule2s = "q(a, b), p(a, b) => False"
+    rule3s = "p(a), r(b, a), p(b) <=> s(a, b)"
+    rule4s = "Either q(a), s(a)"
+    rule5s = "t(a) v p(a) v q(a)"
+
+    a = Term('a', [])
+    b = Term('b', [])
+    p = Term('p', [])
+    q = Term('q', [])
+    r = Term('r', [])
+    t = Term('t', [])
+    s = Term('s', [])
+
+    pa = Atom([p, a])
+    qa = Atom([q, a])
+    sa = Atom([s, a])
+    pab = Atom([p, a, b])
+    qab = Atom([q, a, b])
+    rba = Atom([r, b, a])
+    pb = Atom([p, b])
+    sab = Atom([s, a, b])
+    ta = Atom([t, a])
+
+    rule1 = If([pab], [qa])
+    rule2 = Never([qab, pab])
+    rule3 = Iff([pa, rba, pb], [sab])
+    rule4 = Either([qa, sa])
+    rule5 = Or([ta, pa, qa])
+
+    assert rule1.show() == rule1s
+    assert rule2.show() == rule2s
+    assert rule3.show() == rule3s
+    assert rule4.show() == rule4s
+    assert rule5.show() == rule5s
+
+    dimacs = DimacsIndex(atoms=[pa, qa, pab, qab, rba, pb, sab, ta])
+
