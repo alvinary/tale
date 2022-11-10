aebbe5d219d2c0348aa556c9f46332ec6427ec58
alvinary <alvaro.cuestas.ll@gmail.com>
2022-11-10 20:33:01 -0300

    Draft walker, remove uses of term+

--- programs.py
+++ programs.py
@@ -21,7 +21,9 @@ programs = '''
 
     term = main:name funs:functions
 
-    functions = ("." name)*
+    functions = application | several
+    application = "." fun:name
+    several = "." fun:name rest:application 
 
     rule
         =
@@ -33,7 +35,7 @@ programs = '''
     if = body:atoms '->' head:atoms
     iff = left:atoms '<->' right:atoms
     never = conjuncts:atoms '->' 'False'
-    or = atomdisjunction
+    or = atomd
 
     atom
         =
@@ -56,11 +58,11 @@ programs = '''
     lastd = end:atom
 
     arguments =
-              | many
-              | last
+              | manya
+              | lasta
 
-    many = head:term "," tail:arguments
-    last = end:term
+    manya = head:term "," tail:arguments
+    lasta = end:term
 
     plain = term
 
@@ -72,16 +74,67 @@ programs = '''
     equals = '='
     get = '<='
     ge = '<'
-
 '''
 
-class Interpretation:
-    def operator(self, ast):
+class ProgramSemantics:
+    def name(self, ast):
+        return str(ast)
+    def program(self, ast):
         return ast
-    def comparison(self, ast):
-        return ast
+    def programpart(self, ast):
+        return ast.current + ast.next
+    def finalstatement(self, ast):
+        return [ast.last]
     def statement(self, ast):
-        return ast
+        return [ast]
+    def comment(self, ast):
+        return []
     def term(self, ast):
+        return Term(ast.main, ast.funs)
+    def functions(self, ast):
         return ast
+    def application(self, ast):
+        return [ast.name]
+    def several(self, ast):
+        return [ast.fun] + ast.rest
+    def rule(self, ast):
+        return ast
+    def if(self, ast):
+        return If(ast.body, ast.head)
+    def iff(self, ast):
+        return Iff(ast.left, ast.right)
+    def never(self, ast):
+        return Never(ast.conjuncts)
+    def or(self, ast):
+        return Or(ast)
+    def atomd(self, ast):
+        return ast
+    def manyd(self, ast):
+        return [ast.head] + ast.tail
+    def lastd(self, ast):
+        return [ast.end]
+    def atom(self, ast):
+        return ast
+    def predicate(self, ast):
+        return Atom([ast.term] + ast.args)
+    def arguments(self, ast):
+        return ast
+    def manya(self, ast):
+        return [ast.head] + ast.tail
+    def lasta(self, ast):
+        return [ast.end]
+    def plain(self, ast):
+        return ast
+    def operator(self, ast):
+        return str(ast)
+    def comparison(self, ast):
+        return Comparison(ast.op, ast.left, ast.right)
+    def nequals(self, ast):
+        return '!='
+    def equals(self, ast):
+        return '='
+    def get(self, ast):
+        return '<='
+    def ge(self, ast):
+        return "<"
 
