9f5279453c5bb2d5e44edc930e59204e96606b45
alvinary <alvaro.cuestas.ll@gmail.com>
2022-11-16 10:45:11 -0300

    Add parseGrammar draft

--- models/trees.py
+++ models/trees.py
@@ -24,13 +24,70 @@ actual (a), terminal (a.left, A) <-> leftTerminal (a, A).
 actual (a), terminal (a.right, A) <-> rightTerminal (a, A).
 '''
 
+longRule = lambda l,r,h : f"leftTerminal (a, {l}), rightTerminal (a, {r}) -> terminal (a, {h}).\n"
+shortRule = lambda h, p : f"symbol (a, {p}) -> terminal (a, {h}).\n"
+
 def embedSequences(sequences, tokenLabeling):
     for sequence, identifier in sequences:
         for clause in embedTree(sequence, name=identifier, labeling=tokenLabeling):
             yield clause
 
-def parseGrammar(grammar):
-    pass
+def parseGrammar(text):
+
+    grammar = ""
+    preterminals = set()
+
+    lines = [line.strip() for line in text.split("\n")]
+
+    lastHead = ""
+    ruleCount = 1
+
+    for line in lines:
+
+        ruleCount += 1
+        i = ruleCount
+
+        noHead = "->" == line[0:2]
+        line = line.replace("->", "", 1)
+        tokens = [token.strip() for token in line.split()]
+
+        if len(tokens) == 3 and not noHead:
+            head, left, right = tuple(tokens)
+            last = str(head)
+            line = longRule(left, right, head)
+            grammar = grammar_spec + line
+            preterminals.add(head)
+            preterminals.add(left)
+            preterminals.add(right)
+
+        if len(tokens) == 2 and noHead:
+            left, right = tuple(tokens)
+            head = lastHead
+            line = longRule(left, right, head)
+            grammar = grammar_spec + line
+            preterminals.add(head)
+            preterminals.add(left)
+            preterminals.add(right)
+
+        if len(tokens) == 2 and not noHead:
+            head, right = tuple(tokens)
+            last = head
+            line = shortRule(head, right)
+            grammar = grammar + line
+            preterminals.add(head)
+            preterminals.add(left)
+            preterminals.add(right)
+
+        if len(tokens) == 1 and noHead:
+            right = tokens[0]
+            head = lastHead
+            line = shortRule(head, right)
+            grammar = grammar + line
+            preterminals.add(head)
+            preterminals.add(left)
+            preterminals.add(right)
+
+    return grammar, preterminals
 
 def embedTree(sequence, grammar, name='', labeling=identity):
     clauses = []
@@ -60,7 +117,7 @@ def embedTree(sequence, grammar, name='', labeling=identity):
     for node in leaves:
         treeIndex.valueMap[NODE].append(node)
 
-    for label in nodeLabels:
+    for label in labels:
         treeIndex.valueMap[LABEL].append(label)
 
     nodeVariable = 'a'
