14d016f74d6c8fbbeea2d10761797fe8479dbfb0
alvinary <alvaro.cuestas.ll@gmail.com>
2022-11-19 17:58:03 -0300

    Update tests

--- test/test_formulas.py
+++ test/test_formulas.py
@@ -1,3 +1,4 @@
+from pyclbr import Function
 import pytest
 from pysat.solvers import Solver
 from tale.formulas import *
@@ -27,59 +28,51 @@ def test_terms():
     # Define constants, functions, and a variable 'v',
     # which ranges over functions
     
-    a = "a"
-    b = "b"
-    c = "c"
-    d = "d"
-    f = "f"
-    g = "g"
-    v = "v"
-    
     funs = ["f", "g"]
     cons = [a, b, c, d]
     
     vals = {
-        (f, a) : a,
-        (f, b) : a,
-        (f, c) : d,
-        (f, d) : c,
-        (g, a) : b,
-        (g, b) : a,
-        (g, c) : d,
-        (g, d) : c
+        ('f', 'a') : 'a',
+        ('f', 'b') : 'a',
+        ('f', 'c') : 'd',
+        ('f', 'd') : 'c',
+        ('g', 'a') : 'b',
+        ('g', 'b') : 'a',
+        ('g', 'c') : 'd',
+        ('g', 'd') : 'c'
     }
     
-    assign = Assignment({v : g})
-    varbs = {v : funs}
+    assign = Assignment({'v' : 'g'})
+    varbs = {'v' : 'funs'}
     
     # 1a) Test a single term with a single function, f(a)
     # 1b) Test a compund term, g(f(a))
     
     index = Index(functions=vals, variables=varbs)
-    term1 = Term(a, functions=[f])
-    term2 = Term(a, functions=[f, g])
+    term1 = Term('a', functions=['f'])
+    term2 = Term('a', functions=['f', 'g'])
     
-    val_1a, err_1a = term1.evaluate(index, assign)
-    val_1b, err_1b = term2.evaluate(index, assign)
+    val_1a = term1.evaluate(index, assign)
+    val_1b = term2.evaluate(index, assign)
     
     assert val_1a == a 
-    assert isinstance(err_1a, Ok)
     
     assert val_1b == b
-    assert isinstance(err_1b, Ok)
     
     # Test with assignment
-    term3 = Term(a, functions=[f, g, v])
-    val2, err2 = term3.evaluate(index, assign)
+    term3 = Term('a', functions=['f', 'g', 'v'])
+    val2 = term3.evaluate(index, assign)
     
     assert val2 == a
-    assert isinstance(err2, Ok)
     
     # Test error
     
-    term4 = Term("h", "f")
-    val3, err3 = term4.evaluate(index, assign)
-    assert isinstance(err3, Exception)
+    term4 = Term("gggg", ["f"])
+    try:
+        val = term4.evaluate(index, assign)
+    except FunctionError:
+        assert True
+    
 
 def test_rules():
     
---
+++ test/test_pipeline.py
@@ -0,0 +1,33 @@
+from tale.pipeline import *
+
+program = '''
+shore, docks : place.
+cabbage, goat, wolf : actor.
+farmer : player.
+order i 6 : index.
+
+let at : index, actor -> place.
+let ferry : index, player -> actor.
+
+var p : place.
+var a, b, c : actor.
+var i : index.
+
+let docks.opposite = shore.
+let shore.opposite = docks.
+
+at(i, a, p), at (i, b, p), not at (farmer, p), eats (a, b) -> False.
+
+at(i, a, p), ferry(i, farmer, a) -> at(i.next, a, p.opposite).
+
+at (i0, farmer, shore).
+at (i0, wolf, shore).
+at (i0, cabbage, shore).
+at (i0, goat, shore).
+
+'''
+
+def test_pipeline():
+    for model in pipeline(program):
+        print("\n".join(model))
+        print("\n")
