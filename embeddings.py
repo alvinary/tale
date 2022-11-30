c0fa12a74f3db02205bb580a4b358ae2c12f79e4
alvinary <alvaro.cuestas.ll@gmail.com>
2022-11-30 14:29:45 -0300

    Use atom.negate() in negation()

--- embeddings.py
+++ embeddings.py
@@ -53,13 +53,8 @@ def forbid(index, padding, args, label=''):
 # Clause embeddings
 
 def negation(atoms):
-    notPrefix = 'not'
     for atom in atoms:
-        terms = list(atom.terms)
-        negatedPredicate = Term(f"{notPrefix} {terms[0].term}", [])
-        terms = [negatedPredicate] + terms[1:]
-        negatedAtom = Atom(terms)
-        yield Either([atom, negatedAtom])
+        yield Either([atom, atom.negate()])
 
 def unfold(rule, index):
     for assignment in index.assignments(rule.collect(index)):
