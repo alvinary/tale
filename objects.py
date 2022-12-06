c5de455d7aa15b2fb231da4767e9649013ce29b7
alvinary <alvaro.cuestas.ll@gmail.com>
2022-12-06 12:39:14 -0300

    Replace ',' with ':' in string span

--- objects.py
+++ objects.py
@@ -1,4 +1,5 @@
 class BrokenPrecondition(Exception):
+
     def __init__(self, message, data={}):
         self.message = message
         self.data = data
@@ -9,14 +10,17 @@ class BrokenPrecondition(Exception):
     def __bool__(self):
         return True
 
+
 def remainder(vertex, left, right):
     left = {(a, b) for a, b in left if a != vertex}
     right = {(a, b) for a, b in right if a != vertex}
     return left, right
 
+
 def successors(vertex, relation):
     successors = {b for a, b in relation if a == vertex}
     return successors
+
 
 def bort(vertex, left, right):
     leftSuccessors = successors(vertex, left)
@@ -28,42 +32,55 @@ def bort(vertex, left, right):
     if len(leftSuccessors) == 1:
         leftChild = leftSuccessors.pop()
     elif len(leftSuccessors) > 1:
-        raise BrokenPrecondition(f"More than one left successor found for vertex {vertex} in edge list {left}")
+        raise BrokenPrecondition(
+            f"More than one left successor found for vertex {vertex} in edge list {left}"
+        )
     elif len(leftSuccessors) == 0:
-        raise BrokenPrecondition(f"No left successor found for vertex {vertex}")
+        raise BrokenPrecondition(
+            f"No left successor found for vertex {vertex}")
 
     if len(rightSuccessors) == 1:
         rightChild = leftSuccessors.pop()
     elif len(rightSuccessors) > 1:
-        raise BrokenPrecondition(f"More than one right successor found for vertex {vertex} in edge list {right}")
+        raise BrokenPrecondition(
+            f"More than one right successor found for vertex {vertex} in edge list {right}"
+        )
     elif len(rightSuccessors) == 0:
-        raise BrokenPrecondition(f"No right successor found for vertex {vertex}")
+        raise BrokenPrecondition(
+            f"No right successor found for vertex {vertex}")
 
     leftRemainder, rightRemainder = remainder(vertex, left, right)
 
     if leftSuccessors and rightSuccessors:
-        return Node(vertex, leftChild, rightChild, leftRemainder, rightRemainder)
-    
+        return Node(vertex, leftChild, rightChild, leftRemainder,
+                    rightRemainder)
+
     if not leftSuccessors and not rightSuccessors:
         return Leaf(vertex)
 
+
 class Node():
+
     def __init__(self, vertex, left, right, leftEdges, rightEdges):
         self.vertex = vertex
         self.left = bort(left, leftEdges)
         self.right = bort(right, rightEdges)
 
+
 class Leaf():
+
     def __init__(self, leaf):
         self.leaf = leaf
+
 
 def extract(literal):
     leftParen = literal.index("(")
     rightParen = literal.index(")")
-    pairSpan = literal[leftParen + 1, rightParen]
+    pairSpan = literal[leftParen + 1: rightParen]
     a, b = pairSpan.split(",")
     a, b = a.strip(), b.strip()
     return a, b
+
 
 def reachesBack(vertex, edges):
     current = {vertex}
@@ -73,18 +90,19 @@ def reachesBack(vertex, edges):
         span |= set(current)
     return vertex in span
 
+
 def getTree(model):
 
     isLeft = lambda s: s[:4] == "left"
     isRight = lambda s: s[:5] == "right"
     leftEdges = {extract(literal) for literal in model if isLeft(literal)}
     rightEdges = {extract(literal) for literal in model if isRight(literal)}
-    
+
     edges = leftEdges | rightEdges
     predecessors = {a for a, _ in edges}
     successors = {b for _, b in edges}
     vertices = predecessors | successors
-    
+
     roots = predecessors - successors
     cycleVertices = [v for v in vertices if reachesBack(v, edges)]
 
@@ -93,7 +111,7 @@ def getTree(model):
 
     isTree = rooted and acyclic
 
-    # Lots of heap allocated data structures 
+    # Lots of heap allocated data structures
     # when all this could be on the stack (or
     # at the very least be discarded when the
     # function returns)
