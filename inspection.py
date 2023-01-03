44da9fa2e884fd9817052d8d081b2ffa05565fbf
alvinary <alvaro.cuestas.ll@gmail.com>
2023-01-03 15:45:33 -0300

    Replace tabs with spaces

--- inspection.py
+++ inspection.py
@@ -1,6 +1,6 @@
 
 # The edit distance between talk and also is 2.
-# The edit distance between Score and scores is 2.
+# The edit distance between Score and scores is also 2.
 # We'll use common subsequences between normalized strings as a criterion for similarity.
 
 # Then we'll randomly replace parts of the string with 'aaa's, and compare the histograms with
@@ -8,6 +8,16 @@
 
 # Suppose common(s1, s2) = c
 # - distance from s1 to c, and distance from s2 to c
+
+
+# Replacing with 'a's increases the length of the common subsequence
+# Leaves it the same (if the strings are equal)
+# reduces it 
+
+# replacing with 'a's and 'b's
+
+# set of common substrings (bounded by n**2)
+# Distance between two sparse arrays
 
 def commonSubsequence(first, second):
 
@@ -37,10 +47,10 @@ def commonSubsequence(first, second):
             return popFirst
 
 def testCommon():
-	print(commonSubsequence('AGGTAB', 'GXTXAYB'))
-	print(commonSubsequence('AGXGTAB', 'GXTXAYB'))
-	print(commonSubsequence('AGXGTABY', 'GXTXAYB'))
-	print(commonSubsequence('AGXGTAYB', 'GXTXAYB'))
-	print(commonSubsequence('Score'.lower(), 'scores'.lower()))
-	print(commonSubsequence('talk'.lower(), 'also'.lower()))
-	print(commonSubsequence('talk'.lower(), 'also'.lower()))
+    print(commonSubsequence('AGGTAB', 'GXTXAYB'))
+    print(commonSubsequence('AGXGTAB', 'GXTXAYB'))
+    print(commonSubsequence('AGXGTABY', 'GXTXAYB'))
+    print(commonSubsequence('AGXGTAYB', 'GXTXAYB'))
+    print(commonSubsequence('Score'.lower(), 'scores'.lower()))
+    print(commonSubsequence('talk'.lower(), 'also'.lower()))
+    print(commonSubsequence('talk'.lower(), 'also'.lower()))
