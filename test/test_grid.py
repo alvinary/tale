df32f234dc62a50eecdc3d9dc4d0f538c397d571
alvinary <alvaro.cuestas.ll@gmail.com>
2023-08-16 18:15:16 -0300

    Get path with inspection, remove clutter

--- test/test_grid.py
+++ test/test_grid.py
@@ -1,15 +1,19 @@
+from inspect import getsourcefile
+from os.path import abspath
+
 from tale.objects import getTree
 from tale.pipeline import pipeline
 from tale.formulas import Term
 
 MODELS = 3
 
+path_prefix = str(abspath(getsourcefile(lambda:0)))
+model_path = path_prefix.replace('tale/test/test_grid.py', 'tale/models/grid_trees.tl')
+
 program = ''
-with open('../models/grid_trees.tl', 'r') as grid_trees:
+with open(model_path, 'r') as grid_trees:
     for line in grid_trees:
         program += line
-
-print(program)
 
 def test_grid():
     print('Assembling grid...')
@@ -34,7 +38,10 @@ def test_grid():
     if models:
         for index, model in zip(range(MODELS), models):
             tree = getTree(model)
-            print("\n".join(model))
+            if tree:
+                print(tree.show())
+            else:
+                print("\n".join(model))
 
 if __name__ == '__main__':
     test_grid()
